9cf6b75428bca59dd6fa2a8bc47aede624df0acb
seungo <ilkhso@gmail.com>
2025-01-27 14:23:59 +0900

    feat: event handler

--- study-01/src/components/Button.jsx
+++ study-01/src/components/Button.jsx
@@ -1,6 +1,14 @@
 const Button = ({ text, color, children }) => {
+  const onClickButton = (e) => {
+    console.log(e); // e 는 synthetic based event ( browser들을 통합한 이벤트 인터페이스 )
+    console.log(text);
+  };
+
   return (
-    <button>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+    >
       {text} - {color}
       {children}
     </button>
