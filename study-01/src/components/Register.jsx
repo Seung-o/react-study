258e6a467a515c5777d8425714b6c20a1d7a29e2
seungo <ilkhso@gmail.com>
2025-01-27 15:26:50 +0900

    feat: state manage (1)

--- study-01/src/components/Register.jsx
+++ study-01/src/components/Register.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+// 회원 가입 폼
+
 const Register = () => {
-  return <div>register</div>;
+  const [name, setName] = useState("");
+  const [birth, setBirth] = useState("");
+  const [country, setCountry] = useState("");
+  const [bio, setBio] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeBirth = (e) => {
+    setBirth(e.target.value);
+  };
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const onChangeBio = (e) => {
+    setBio(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+      </div>
+      <div>
+        <input value={birth} type="date" onChange={onChangeBirth} />
+      </div>
+      <div>
+        <select value={country} onChange={onChangeCountry}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+      <div>
+        <textarea
+          value={bio}
+          onChange={onChangeBio}
+          placeholder="자기소개를 입력해주세요."
+        />
+      </div>
+    </>
+  );
 };
 
 export default Register;
