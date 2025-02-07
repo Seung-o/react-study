a53489d4e6dd523f650d2beb47a160125d321396
seungo <ilkhso@gmail.com>
2025-02-07 23:24:44 +0900

    feat: add link

--- study-04/src/App.jsx
+++ study-04/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
@@ -7,18 +7,21 @@ import NotFound from "./pages/NotFound";
 
 // 1. "/": 모든 일기를 조회하는 Home 페이지
 // 2. "/new": 새 일기를 작성하는 페이지
-// 3. "/diary": 일기 상세를 조회하는 페이지
+// 3. "/diary/:id": 일기 상세를 조회하는 페이지
 function App() {
   return (
     <>
       <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/diary" element={<Diary />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Link to="/">Home</Link>
+        <Link to="/new">New</Link>
+        <Link to="/diary">Diary</Link>
       </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
--- study-04/src/pages/Diary.jsx
+++ study-04/src/pages/Diary.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
+  const params = useParams();
+  console.log(params);
+
   return (
     <div>
-      <div>Emotion Diary</div>
+      <div>{params.id} 번째 일기</div>
     </div>
   );
 };
