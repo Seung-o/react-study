cc83e909413fff7c8883174d5d26bbe58e1e8409
seungo <ilkhso@gmail.com>
2025-02-07 23:14:03 +0900

    feat: add react route dom

--- study-04/src/App.jsx
+++ study-04/src/App.jsx
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Diary from "./pages/Diary";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
 
+// 1. "/": 모든 일기를 조회하는 Home 페이지
+// 2. "/new": 새 일기를 작성하는 페이지
+// 3. "/diary": 일기 상세를 조회하는 페이지
 function App() {
   return (
     <>
-      <div>Emotion Diary</div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ study-04/src/pages/Diary.jsx
@@ -0,0 +1,9 @@
+const Diary = () => {
+  return (
+    <div>
+      <div>Emotion Diary</div>
+    </div>
+  );
+};
+
+export default Diary;
